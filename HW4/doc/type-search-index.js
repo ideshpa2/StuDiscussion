6d7eddca227a23844769222b9ce05da6c6ab739e
ideshpa2 <143305807+ideshpa2@users.noreply.github.com>
2025-08-12 03:04:50 -0700

    Delete HW4 directory

--- HW4/doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"application","l":"ReviewerHomePage"},{"p":"application","l":"StaffHomePage"},{"p":"application","l":"StudentHomePage"}];updateSearchResults();
